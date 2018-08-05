819e635394a809a7c49ec446bf3e86d8d4c2227f
Zhengwei.Ren <zhengwei.ren@openjawtech.com>
2018-08-05 23:19:00 +0800

    Optimise the publish information.

--- src/main/java/info/woody/api/intellij/plugin/csct/CodeStyleCheckRuleImpl.groovy
+++ src/main/java/info/woody/api/intellij/plugin/csct/CodeStyleCheckRuleImpl.groovy
@@ -529,3 +529,4 @@ class CodeStyleCheckRuleImpl extends CodeStyleCheckRule {
 // TODO(OPTIONAL): Rationale: Each instance variable gets initialized twice, to the same value.
 // TODO: Separate author report from file report
 // TODO: Asynchronously create reports to improve the display performance
+// TODO: Advice define a variable for complex boolean expression to improve the readability: ^[\t ]*\bif[^{\n]{100,}$
